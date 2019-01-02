6500cffcd9c5f71b1ba5e3ff7da2f6064ac394c2
alexmozheyko <aleksmozheiko94@gmail.com>
2019-01-02 16:22:44 +0300

    Add hash method to HashTable class

--- hash-table/main.js
+++ hash-table/main.js
@@ -1,5 +1,35 @@
 (function () {
 
-    
+    class HashTable {
+        constructor(size) {
+            this.buckets    = new Array(size);
+            this.numBuckets = size;
+        }
+
+        hash(key) {
+            let total = 0;
+            let keyLength = key.length;
+
+            for (var i = 0; i < keyLength; i++) {
+                total += key.charCodeAt(i);
+            }
+
+            let bucket = total % this.numBuckets;
+
+            return bucket;
+        }
+    }
+
+    class HashNode {
+        constructor(key, value, next = null) {
+            this.key   = key;
+            this.value = value;
+            this.next  = next;
+        }
+    }
+
+    let myHT = new HashTable(30);
+
+    console.log(myHT.hash('Becca'));
 
 })();
