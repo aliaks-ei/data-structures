a8c41d454cc1345c8cd5ef5ba0acfcc337992d04
Aliaksei Mazheika <aliaksei.mazheika@emma-sleep.com>
2023-08-25 14:47:32 +0100

    Updated unit tests

--- src/hash-table/tests/index.spec.ts
+++ src/hash-table/tests/index.spec.ts
@@ -1,15 +1,15 @@
-import HashTable from '..';
+import HashTable from "..";
 
 /** Test cases on get() and set() method */
-describe('HashTable:', () => {
-  describe('get() and set() methods', () => {
-    test('should get correct values', () => {
-      const hashTable = new HashTable(3);
-  
+describe("HashTable:", () => {
+  describe("get() and set() methods", () => {
+    test("should get correct values", () => {
+      const hashTable = new HashTable(100);
+
       hashTable.set("Canada", 300);
       hashTable.set("France", 100);
       hashTable.set("Spain", 110);
-  
+
       expect(hashTable.get("Canada")).toBe(300);
       expect(hashTable.get("France")).toBe(100);
       expect(hashTable.get("Spain")).toBe(110);
@@ -17,15 +17,15 @@ describe('HashTable:', () => {
     });
   });
 
-  describe('remove() methods', () => {
-    test('should remove correct values', () => {
-      const hashTable = new HashTable(3);
-  
+  describe("remove() methods", () => {
+    test("should remove correct values", () => {
+      const hashTable = new HashTable(100);
+
       hashTable.set("Andrew", 300);
       hashTable.set("Florence", 100);
       hashTable.set("Paul", 110);
-  
-      expect(hashTable.remove("Canada")).toBeTruthy();
+
+      expect(hashTable.remove("Andrew")).toBeTruthy();
       expect(hashTable.remove("Florence")).toBeTruthy();
       expect(hashTable.remove("Paul")).toBeTruthy();
       expect(hashTable.remove("Kate")).toBeFalsy();
